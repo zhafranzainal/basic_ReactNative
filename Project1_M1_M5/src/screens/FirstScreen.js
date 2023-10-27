675fa3027bdfc3c7413b400d5009641a297002d3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-28 06:21:47 +0800

    Create view and text components

---
+++ Project1_M1_M5/src/screens/FirstScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from 'react-native';
+import { exp } from "react-native-reanimated";
+
+const FirstScreen = () => {
+
+    return (
+        <View>
+            <Text>
+                Hello World!
+            </Text>
+        </View>
+    )
+
+}
+
+export default FirstScreen;
