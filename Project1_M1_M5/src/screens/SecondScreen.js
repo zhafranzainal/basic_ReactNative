d37a8d5564210b6f21931bc49dd389a49a53d521
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-05 04:07:00 +0800

    Manipulate margin, border, and padding of a component

--- Project1_M1_M5/src/screens/SecondScreen.js
+++ Project1_M1_M5/src/screens/SecondScreen.js
@@ -6,13 +6,13 @@ const SecondScreen = () => {
     return (
         <View style={{ flex: 1, backgroundColor: 'lavender', justifyContent: "center", alignItems: "center" }}>
 
-            <View style={{ backgroundColor: 'skyblue' }}>
+            <View style={{ backgroundColor: 'skyblue', margin: 20, padding: 7, borderWidth: 1, borderColor: 'red', borderStyle: 'dashed', borderRadius: 7 }}>
                 <Text>
                     Mobile Legends
                 </Text>
             </View>
 
-            <View style={{ backgroundColor: 'cornflowerblue' }}>
+            <View style={{ backgroundColor: 'cornflowerblue', padding: 7, borderWidth: 1, borderColor: 'red', borderStyle: 'dotted', borderRadius: 7 }}>
                 <Text>
                     PUBG
                 </Text>
