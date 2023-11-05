6bc8ee78076d7de942027be51d3649a4918386ce
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-05 09:53:18 +0800

    Style text

--- Project1_M1_M5/src/screens/SecondScreen.js
+++ Project1_M1_M5/src/screens/SecondScreen.js
@@ -7,14 +7,23 @@ const SecondScreen = () => {
         <View style={{ flex: 1, backgroundColor: 'lavender', justifyContent: "center", alignItems: "center" }}>
 
             <View style={{ backgroundColor: 'skyblue', margin: 20, padding: 7, borderWidth: 1, borderColor: 'red', borderStyle: 'dashed', borderRadius: 7 }}>
-                <Text>
+                <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
                     Mobile Legends
+                </Text>
+                <Text style={{ textAlign: 'justify' }}>
+                    <Text style={{ color: 'purple' }}>Mobile Legends</Text> is a multiplayer online battle arena (MOBA) game. The two
+                    opposing teams fight to reach and destroy the enemy's base while defending
+                    their own base for control of a path.
                 </Text>
             </View>
 
-            <View style={{ backgroundColor: 'cornflowerblue', padding: 7, borderWidth: 1, borderColor: 'red', borderStyle: 'dotted', borderRadius: 7 }}>
-                <Text>
+            <View style={{ backgroundColor: 'cornflowerblue', marginLeft: 17, marginRight: 17, padding: 7, borderWidth: 1, borderColor: 'red', borderStyle: 'dotted', borderRadius: 7 }}>
+                <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
                     PUBG
+                </Text>
+                <Text style={{ textAlign: 'justify' }}>
+                    <Text style={{ color: 'purple' }}>PlayerUnknown's Battlegrounds</Text>, better known as PUBG, is a multiplayer battle royale
+                    game in which players drop onto an island and fight to be the last one left standing..
                 </Text>
             </View>
 
