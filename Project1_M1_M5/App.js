6a138c7cba4f1c3db433bff5ac4cec5cdc64a5c6
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-10 19:23:13 +0800

    Create third screen

--- Project1_M1_M5/App.js
+++ Project1_M1_M5/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FirstScreen from "./src/screens/FirstScreen";
 import SecondScreen from "./src/screens/SecondScreen";
+import ThirdScreen from "./src/screens/ThirdScreen";
 
 const App = () => {
 
   return (
     <SafeAreaProvider>
-      <SecondScreen />
+      <ThirdScreen />
     </SafeAreaProvider>
   )
 
---
+++ Project1_M1_M5/src/screens/ThirdScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const ThirdScreen = () => {
+    return (
+        <View>
+            <Text>
+                Pikachu
+            </Text>
+        </View>
+    )
+};
+
+export default ThirdScreen;
