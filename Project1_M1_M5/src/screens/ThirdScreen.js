e7ae63d0805e2fc519b2e5ae1d7a6ed7726bb7c3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-11 01:53:29 +0800

    Add scroll view component

--- Project1_M1_M5/src/screens/ThirdScreen.js
+++ Project1_M1_M5/src/screens/ThirdScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 
 const ThirdScreen = () => {
     return (
@@ -29,74 +29,76 @@ const ThirdScreen = () => {
 
 const AddImage = () => {
     return (
-        <View style={{ alignItems: 'center' }}>
+        <ScrollView>
+            <View style={{ alignItems: 'center' }}>
 
-            <View style={{ margin: 8, padding: 8 }}>
-                <Image style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    borderColor: 'green',
-                    backgroundColor: 'aliceblue'
-                }}
-                    source={require('../../assets/images/pikachu.png')}
-                />
+                <View style={{ margin: 8, padding: 8 }}>
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={require('../../assets/images/pikachu.png')}
+                    />
+                </View>
+
+                <View style={{ margin: 8, padding: 8 }}>
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={require('../../assets/images/flareon.png')}
+                    />
+                </View>
+
+                <View style={{ margin: 8, padding: 8 }}>
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={require('../../assets/images/rapidash.png')}
+                    />
+                </View>
+
+                <View style={{ margin: 8, padding: 8 }}>
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={{ uri: 'https://i.ibb.co/bLz74hj/psyduck.png' }}
+                    />
+                </View>
+
+                <View style={{ margin: 8, padding: 8 }}>
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={{ uri: 'https://i.ibb.co/GQBLC7r/bulbasaur.png' }}
+                    />
+                </View>
+
             </View>
-
-            <View style={{ margin: 8, padding: 8 }}>
-                <Image style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    borderColor: 'green',
-                    backgroundColor: 'aliceblue'
-                }}
-                    source={require('../../assets/images/flareon.png')}
-                />
-            </View>
-
-            <View style={{ margin: 8, padding: 8 }}>
-                <Image style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    borderColor: 'green',
-                    backgroundColor: 'aliceblue'
-                }}
-                    source={require('../../assets/images/rapidash.png')}
-                />
-            </View>
-
-            <View style={{ margin: 8, padding: 8 }}>
-                <Image style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    borderColor: 'green',
-                    backgroundColor: 'aliceblue'
-                }}
-                    source={{ uri: 'https://i.ibb.co/bLz74hj/psyduck.png' }}
-                />
-            </View>
-
-            <View style={{ margin: 8, padding: 8 }}>
-                <Image style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    borderColor: 'green',
-                    backgroundColor: 'aliceblue'
-                }}
-                    source={{ uri: 'https://i.ibb.co/GQBLC7r/bulbasaur.png' }}
-                />
-            </View>
-
-        </View>
+        </ScrollView>
     )
 };
 
