4cadaa134cf6faeafa68ef1f84d21f269cb0f8a5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-11 21:06:17 +0800

    Add images with description

--- Project1_M1_M5/src/screens/ThirdScreen.js
+++ Project1_M1_M5/src/screens/ThirdScreen.js
@@ -118,6 +118,7 @@ const ChallengeScreen = () => {
 
             <ScrollView>
 
+                {/* Pikachu */}
                 <View style={{
                     margin: 8,
                     padding: 8,
@@ -147,6 +148,146 @@ const ChallengeScreen = () => {
                             Abilities: Static
                             {`\n`}
                             Weaknesses: Ground
+                        </Text>
+                    </View>
+
+                </View>
+
+                {/* Flareon */}
+                <View style={{
+                    margin: 8,
+                    padding: 8,
+                    flexDirection: 'row',
+                }}>
+
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 20,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={require('../../assets/images/flareon.png')}
+                    />
+
+                    <View style={{
+                        marginLeft: 20,
+                        justifyContent: 'center',
+                    }}>
+                        <Text>
+                            Name: Flareon
+                            {`\n`}
+                            Category: Flame
+                            {`\n`}
+                            Abilities: Flash Fire
+                            {`\n`}
+                            Weaknesses: Water, Ground, Rock
+                        </Text>
+                    </View>
+
+                </View>
+
+                {/* Rapidash */}
+                <View style={{
+                    margin: 8,
+                    padding: 8,
+                    flexDirection: 'row',
+                }}>
+
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 20,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={require('../../assets/images/rapidash.png')}
+                    />
+
+                    <View style={{
+                        marginLeft: 20,
+                        justifyContent: 'center',
+                    }}>
+                        <Text>
+                            Name: Rapidash
+                            {`\n`}
+                            Category: Fire Horse
+                            {`\n`}
+                            Abilities: Run Away, Flash Fire
+                            {`\n`}
+                            Weaknesses: Water, Ground, Rock
+                        </Text>
+                    </View>
+
+                </View>
+
+                {/* Psyduck */}
+                <View style={{
+                    margin: 8,
+                    padding: 8,
+                    flexDirection: 'row',
+                }}>
+
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 20,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={{ uri: 'https://i.ibb.co/bLz74hj/psyduck.png' }}
+                    />
+
+                    <View style={{
+                        marginLeft: 20,
+                        justifyContent: 'center',
+                    }}>
+                        <Text>
+                            Name: Psyduck
+                            {`\n`}
+                            Category: Duck
+                            {`\n`}
+                            Abilities: Dump, Cloud Nine
+                            {`\n`}
+                            Weaknesses: Grass, Electric
+                        </Text>
+                    </View>
+
+                </View>
+
+                {/* Bulbasaur */}
+                <View style={{
+                    margin: 8,
+                    padding: 8,
+                    flexDirection: 'row',
+                }}>
+
+                    <Image style={{
+                        width: 150,
+                        height: 150,
+                        borderRadius: 20,
+                        borderWidth: 1,
+                        borderColor: 'green',
+                        backgroundColor: 'aliceblue'
+                    }}
+                        source={{ uri: 'https://i.ibb.co/GQBLC7r/bulbasaur.png' }}
+                    />
+
+                    <View style={{
+                        marginLeft: 20,
+                        justifyContent: 'center',
+                    }}>
+                        <Text>
+                            Name: Bulbasaur
+                            {`\n`}
+                            Category: Seed
+                            {`\n`}
+                            Abilities: Overgrow
+                            {`\n`}
+                            Weaknesses: Fire, Psychic, Flying, Ice
                         </Text>
                     </View>
 
