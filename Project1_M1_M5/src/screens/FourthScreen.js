fdb713c1b9453aa3d50c6efbd5e518cc68783f9c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 23:16:27 +0800

    Add text input

--- Project1_M1_M5/src/screens/FourthScreen.js
+++ Project1_M1_M5/src/screens/FourthScreen.js
@@ -1,31 +1,62 @@
 import React from "react";
-import { View, ImageBackground, Text } from 'react-native';
+import { View, ImageBackground, Text, TextInput } from 'react-native';
 
 const FourthScreen = () => {
     return (
         <View style={{ margin: 16 }}>
 
-            <ImageBackground style={{
-                width: '100%',
-                height: 300,
-                justifyContent: 'flex-end',
-                alignItems: 'flex-end'
-            }}
-                imageStyle={{ borderRadius: 10, opacity: 0.5 }}
-                source={{ uri: 'https://wallpaperaccess.com/full/250180.jpg' }}
+            <ImageBackground
+
+                style={{
+                    width: '100%',
+                    height: 300,
+                    justifyContent: 'flex-end',
+                    alignItems: 'flex-end'
+                }}
+
+                imageStyle={{
+                    borderRadius: 10,
+                    opacity: 0.5
+                }}
+
+                source={{
+                    uri: 'https://wallpaperaccess.com/full/250180.jpg'
+                }}
+
             >
 
-                <Text style={{
-                    backgroundColor: 'mistyrose',
-                    padding: 8,
-                    margin: 8,
-                    fontSize: 18,
-                    fontWeight: 'bold',
-                }}>
+                <Text
+                    style={{
+                        backgroundColor: 'mistyrose',
+                        padding: 8,
+                        margin: 8,
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                    }}
+                >
                     Disneyland
                 </Text>
 
             </ImageBackground>
+
+            <Text style={{ marginTop: 12, marginBottom: 12 }}>
+                Description
+            </Text>
+
+            <TextInput
+
+                style={{
+                    padding: 10,
+                    borderWidth: 1,
+                    height: 40,
+                    fontSize: 16
+                }}
+
+                placeholder='About the place'
+                multiline={true}
+                keyboardType='default'
+
+            />
 
         </View >
     )
