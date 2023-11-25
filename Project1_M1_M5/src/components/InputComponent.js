64b31f7a06df0daedbb0dae12f1f4973bc85c884
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 23:59:17 +0800

    Create reusable component

---
+++ Project1_M1_M5/src/components/InputComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, TextInput } from 'react-native';
+
+export const InputComponent = (props) => {
+
+    const { title, height } = props;
+
+    return (
+        <View>
+
+            <Text style={{ marginTop: 12, marginBottom: 12 }}>
+                {{ title }}
+            </Text>
+
+            <TextInput
+
+                style={{
+                    padding: 10,
+                    borderWidth: 1,
+                    height: height,
+                    fontSize: 16
+                }}
+
+                {...props}
+
+            />
+
+        </View >
+    )
+
+};
