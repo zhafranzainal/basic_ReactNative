c292efa3ce132d37e2229e2327fd393a36415547
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 22:58:49 +0800

    Add text in image

--- Project1_M1_M5/src/screens/FourthScreen.js
+++ Project1_M1_M5/src/screens/FourthScreen.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { View, ImageBackground } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
 
 const FourthScreen = () => {
     return (
         <View style={{ margin: 16 }}>
 
-            <ImageBackground
-
-                style={{
-                    width: '100%',
-                    height: 300,
-                    justifyContent: 'flex-end',
-                    alignItems: 'flex-start'
-                }}
-
+            <ImageBackground style={{
+                width: '100%',
+                height: 300,
+                justifyContent: 'flex-end',
+                alignItems: 'flex-start'
+            }}
                 imageStyle={{ borderRadius: 10 }}
-                source={{ uri: 'https://wallpaperaccess.com/full/250180.jpg' }}>
+                source={{ uri: 'https://wallpaperaccess.com/full/250180.jpg' }}
+            >
+
+                <Text style={{
+                    backgroundColor: 'mistyrose',
+                    padding: 8,
+                    margin: 8,
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                }}>
+                    Disneyland
+                </Text>
 
             </ImageBackground>
 
