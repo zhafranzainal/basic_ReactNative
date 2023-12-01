633f10fa209cd99ef4a1b769eb1a4c396dfff59e
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-02 02:18:09 +0800

    Update InstagramHomeScreen.js

--- Project1_M1_M5/src/screens/InstagramHomeScreen.js
+++ Project1_M1_M5/src/screens/InstagramHomeScreen.js
@@ -5,28 +5,34 @@ import { Icon } from 'react-native-elements';
 const InstagramHomeScreen = () => {
     return (
         <View style={{ flex: 1 }}>
+
             <View style={{
                 flexDirection: 'row',
                 justifyContent: 'space-between'
             }}>
-                <Text
-                    style={{
-                        fontFamily: 'StyleScript-Regular',
-                        fontSize: 40, marginLeft: 16
-                    }}>
+
+                <Text style={{
+                    fontFamily: 'StyleScript-Regular',
+                    fontSize: 40,
+                    marginLeft: 16
+                }}>
                     Instagram
                 </Text>
+
                 <View style={{
                     flexDirection: 'row',
-                    alignItems: 'center', marginRight: 8
+                    alignItems: 'center',
+                    marginRight: 8
                 }}>
+
                     <View style={{ margin: 8 }}>
-                        <Icon name='plus-square-o'
-                            type='font-awesome' size={26}
-                        />
+                        <Icon name='plus-square-o' type='font-awesome' size={26} />
                     </View>
+
                 </View>
+
             </View >
+
         </View >
     )
 };
