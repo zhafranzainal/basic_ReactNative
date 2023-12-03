fcf4ee6b7ad1af4d3e99c8cf53639ada6c7e8e4d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-03 13:43:21 +0800

    Implement hook (useEffect)

--- GuessTheCountry_M6_M7/src/screens/HomeScreen.js
+++ GuessTheCountry_M6_M7/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
@@ -12,6 +12,11 @@ const HomeScreen = () => {
     const increaseNumber = () => {
         setNumber(number + 1);
     };
+
+    useEffect(() => {
+        setNumber(10);
+        alert('You can increase or decrease the number!');
+    }, []);
 
     return (
         <View style={{
