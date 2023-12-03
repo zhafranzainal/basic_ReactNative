e91d3a991393f264e5ac54950b4cb4608bd60460
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-03 13:55:46 +0800

    Create Guess Country screen

--- GuessTheCountry_M6_M7/App.tsx
+++ GuessTheCountry_M6_M7/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from './src/screens/HomeScreen'
+import GuessTheCountryScreen from "./src/screens/GuessTheCountryScreen";
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      <GuessTheCountryScreen />
     </SafeAreaProvider>
   )
 }
---
+++ GuessTheCountry_M6_M7/src/screens/GuessTheCountryScreen.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+
+const GuessTheCountryScreen = () => {
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+
+            <Text style={{
+                textDecorationLine: 'underline',
+                fontSize: 28,
+                fontFamily: 'serif',
+                marginBottom: 16
+            }}>
+                Guess The Country
+            </Text>
+
+            <Image
+                style={{
+                    width: 250,
+                    height: 250,
+                    borderRadius: 10
+                }}
+                source={{
+                    uri: 'https://wallpaperaccess.com/full/180136.jpg'
+                }}
+            />
+
+            <View style={{
+                margin: 8,
+                backgroundColor: 'lavender',
+                padding: 4,
+                borderWidth: 1
+            }}>
+                <Text style={{ fontSize: 18 }}>
+                    Big Ben
+                </Text>
+            </View>
+
+            <View style={{
+                flexDirection: 'row',
+                margin: 8
+            }}>
+
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        width: '50%',
+                    }}
+                    placeholder="Write your answer"
+                />
+
+                <TouchableOpacity style={{
+                    borderWidth: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 10,
+                    padding: 8,
+                    marginLeft: 8,
+                    marginBottom: 8,
+                    marginTop: 8,
+                    backgroundColor: 'skyblue'
+                }}>
+
+                    <Text style={{ fontSize: 18 }}>
+                        Submit
+                    </Text>
+
+                </TouchableOpacity >
+
+            </View >
+
+        </View >
+    )
+};
+
+export default GuessTheCountryScreen;
