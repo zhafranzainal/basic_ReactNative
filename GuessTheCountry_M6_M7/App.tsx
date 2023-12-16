4b56a02ee6f5769d851d1e4e5e561a68b75f602f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-17 01:49:39 +0800

    Create win screen

--- GuessTheCountry_M6_M7/App.tsx
+++ GuessTheCountry_M6_M7/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from './src/screens/HomeScreen'
 import GuessTheCountryScreen from "./src/screens/GuessTheCountryScreen";
+import WinScreen from "./src/screens/WinScreen";
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <GuessTheCountryScreen />
+      <WinScreen />
     </SafeAreaProvider>
   )
 }
---
+++ GuessTheCountry_M6_M7/src/screens/WinScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, ImageBackground, Image, TouchableOpacity, Text } from 'react-native';
+
+const WinScreen = () => {
+
+    return (
+        <View style={{ flex: 1 }}>
+            <ImageBackground
+                source={{
+                    uri: 'https://wallpaperaccess.com/full/154959.jpg'
+                }}
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                imageStyle={{ opacity: 0.5 }}
+            >
+
+                <Image
+                    source={require('../../assets/images/congratulations.png')}
+                    style={{
+                        width: '100%',
+                        height: 200
+                    }}
+                />
+
+                <View
+                    style={{
+                        borderWidth: 1,
+                        backgroundColor: 'olivedrab',
+                        padding: 8,
+                        borderRadius: 10
+                    }}
+                >
+
+                    <TouchableOpacity>
+                        <Text style={{
+                            textTransform: 'uppercase',
+                            fontSize: 24,
+                            fontWeight: 'bold',
+                            color: 'black'
+                        }}>
+                            Play Again
+                        </Text>
+                    </TouchableOpacity>
+
+                </View >
+
+            </ImageBackground >
+        </View >
+    )
+
+};
+
+export default WinScreen;
