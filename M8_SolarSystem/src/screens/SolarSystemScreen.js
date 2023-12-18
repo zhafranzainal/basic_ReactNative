faec11a9d1443792eb5103052e56716706f95e1a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-18 15:33:18 +0800

    Add SolarSystemScreen

---
+++ M8_SolarSystem/src/screens/SolarSystemScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, ImageBackground, Image, ScrollView, TouchableOpacity } from 'react-native';
+
+const SolarSystemScreen = () => {
+    return (
+        <View>
+            <Text>SOLAR SYSTEM</Text>
+        </View>
+    )
+};
+
+export default SolarSystemScreen;
