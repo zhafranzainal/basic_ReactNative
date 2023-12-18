2a10db789b8a70246d4da5f4775af22b4e3168ef
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-18 16:41:00 +0800

    Add clickable planet to display alert

--- M8_SolarSystem/src/components/PlanetComponent.js
+++ M8_SolarSystem/src/components/PlanetComponent.js
@@ -1,52 +1,59 @@
 import React from "react";
-import { View, Image, Text } from 'react-native';
+import { TouchableOpacity, View, Image, Text } from 'react-native';
 
 export const PlanetComponent = (props) => {
 
     const { planetImage, planetName, planetDescription } = props;
 
+    const clickedPlanet = (planet) => {
+        capitalizedPlanet = planet[0].toUpperCase() + planet.slice(1);
+        alert(`You clicked ${capitalizedPlanet}!`);
+    };
+
     return (
-        <View style={{
-            flexDirection: 'row',
-            backgroundColor: 'wheat',
-            margin: 16,
-            padding: 16,
-            borderRadius: 20,
-            borderColor: 'green',
-            borderWidth: 1
-        }}>
-
-            <Image
-
-                style={{
-                    width: 100,
-                    height: 100,
-                }}
-
-                source={planetImage}
-
-            />
-
+        <TouchableOpacity onPress={() => clickedPlanet(planetName)}>
             <View style={{
-                flex: 1,
-                marginLeft: 8,
-                marginRight: 8,
-                justifyContent: 'center'
+                flexDirection: 'row',
+                backgroundColor: 'wheat',
+                margin: 16,
+                padding: 16,
+                borderRadius: 20,
+                borderColor: 'green',
+                borderWidth: 1
             }}>
 
-                <Text style={{
-                    fontSize: 20,
-                    fontWeight: 'bold',
-                    textTransform: 'capitalize'
-                }}>
-                    {planetName}
-                </Text>
+                <Image
 
-                <Text>{planetDescription}</Text>
+                    style={{
+                        width: 100,
+                        height: 100,
+                    }}
+
+                    source={planetImage}
+
+                />
+
+                <View style={{
+                    flex: 1,
+                    marginLeft: 8,
+                    marginRight: 8,
+                    justifyContent: 'center'
+                }}>
+
+                    <Text style={{
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        textTransform: 'capitalize'
+                    }}>
+                        {planetName}
+                    </Text>
+
+                    <Text>{planetDescription}</Text>
+
+                </View>
 
             </View>
-
-        </View>
+        </TouchableOpacity>
     )
 
 };
