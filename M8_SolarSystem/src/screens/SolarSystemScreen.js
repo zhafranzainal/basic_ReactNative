86bc9d6aa954bc096b32b2c9abe5d4bd910f1d88
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-18 15:59:42 +0800

    Update SolarSystemScreen.js

--- M8_SolarSystem/src/screens/SolarSystemScreen.js
+++ M8_SolarSystem/src/screens/SolarSystemScreen.js
@@ -3,8 +3,64 @@ import { View, Text, ImageBackground, Image, ScrollView, TouchableOpacity } from
 
 const SolarSystemScreen = () => {
     return (
-        <View>
-            <Text>SOLAR SYSTEM</Text>
+        <View style={{ flex: 1 }}>
+
+            <View style={{
+                padding: 16,
+                alignItems: 'center'
+            }}>
+                <Text style={{
+                    fontSize: 30,
+                    fontFamily: 'serif',
+                    textDecorationLine: 'underline',
+                    textTransform: 'uppercase',
+                    color: 'black'
+                }}>
+                    Solar System
+                </Text>
+            </View>
+
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: 'wheat',
+                margin: 16,
+                padding: 16,
+                borderRadius: 20,
+                borderColor: 'green',
+                borderWidth: 1
+            }}>
+
+                <Image
+
+                    style={{
+                        width: 100,
+                        height: 100,
+                    }}
+
+                    source={require('../../assets/images/mercury.png')}
+
+                />
+
+                <View style={{
+                    flex: 1,
+                    marginLeft: 8,
+                    marginRight: 8,
+                    justifyContent: 'center'
+                }}>
+
+                    <Text style={{
+                        fontSize: 20,
+                        fontWeight: 'bold'
+                    }}>
+                        Mercury
+                    </Text>
+
+                    <Text>Mercury is the fastest</Text>
+
+                </View>
+
+            </View>
+
         </View>
     )
 };
