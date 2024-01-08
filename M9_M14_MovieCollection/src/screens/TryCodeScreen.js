1b7be3ac2f24df3e2aae6f082014eaf87e943073
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-09 01:49:31 +0800

    Add text to top of the screen

--- M9_M14_MovieCollection/src/screens/TryCodeScreen.js
+++ M9_M14_MovieCollection/src/screens/TryCodeScreen.js
@@ -7,6 +7,13 @@ const TryCodeScreen = () => {
     return (
         <View style={{ flex: 1 }}>
 
+            <Text>
+                An array of objects lets you store multiple values in a single variable. It
+                stores a fixed-size sequential collection of elements of the same type. An
+                array is used to store a collection of data, but it is often more useful to
+                think of an array as a collection of variables of the same type.
+            </Text>
+
             <FlatList
                 contentContainerStyle={{ padding: 8 }}
                 data={userData}
