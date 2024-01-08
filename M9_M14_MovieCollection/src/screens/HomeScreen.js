9dbf2e0fa9c81bb18d418143751535e111fb738b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-09 02:08:49 +0800

    Add styles using StyleSheet

--- M9_M14_MovieCollection/src/screens/HomeScreen.js
+++ M9_M14_MovieCollection/src/screens/HomeScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList, Image } from 'react-native';
+import { movieData } from '../../data/MovieData';
 
 const HomeScreen = () => {
 
     return (
         <View style={styles.mainContainer}>
-            <Text>Home Screen</Text>
+
+            <FlatList
+                data={movieData}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.flatListContainer}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.dataContainer}>
+                            <Image
+                                style={styles.movieImage}
+                                source={{ uri: item.imageLink }}
+                            />
+                        </View>
+                    )
+                }}
+            />
+
         </View>
     )
 
@@ -14,6 +31,17 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1
+    },
+    flatListContainer: {
+        padding: 8
+    },
+    dataContainer: {
+        margin: 8
+    },
+    movieImage: {
+        width: 130,
+        height: 200,
+        borderRadius: 10
     },
 });
 
