27bcc559c7356c95f1dbf518b6644f1095cc4ff6
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-04 08:24:23 +0800

    Add image to detail movie screen

--- M9_M14_MovieCollection/src/screens/DetailMovieScreen.js
+++ M9_M14_MovieCollection/src/screens/DetailMovieScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
 
 const DetailMovieScreen = (props) => {
@@ -14,11 +14,40 @@ const DetailMovieScreen = (props) => {
     }, []);
 
     return (
-        <View>
-            <Text>Detail Movie</Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.movieContainer}>
+                <View style={styles.middle}>
+
+                    <Image
+                        style={styles.image}
+                        source={{ uri: movie.imageLink }}
+                    />
+
+                </View>
+            </View>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    },
+    movieContainer: {
+        margin: 8,
+        padding: 8
+    },
+    middle: {
+        alignItems: 'center'
+    },
+    image: {
+        width: 200,
+        height: 300,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: '#ffbe7bff'
+    }
+});
 
 export default DetailMovieScreen;
