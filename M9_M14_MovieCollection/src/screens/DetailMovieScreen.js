21a667a829976af5027cc9aafc7dc62fc2b7ba8a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-04 08:26:54 +0800

    Add movie title to detail movie screen

--- M9_M14_MovieCollection/src/screens/DetailMovieScreen.js
+++ M9_M14_MovieCollection/src/screens/DetailMovieScreen.js
@@ -23,6 +23,10 @@ const DetailMovieScreen = (props) => {
                         source={{ uri: movie.imageLink }}
                     />
 
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>{movie.title}</Text>
+                    </View>
+
                 </View>
             </View>
         </View>
@@ -47,6 +51,20 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 3,
         borderColor: '#ffbe7bff'
+    },
+    titleContainer: {
+        marginTop: 8,
+        marginBottom: 8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        padding: 8,
+        backgroundColor: 'salmon',
+        borderRadius: 10,
+        color: 'white',
     }
 });
 
