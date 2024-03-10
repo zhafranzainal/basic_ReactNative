6a49ca7c6052238619b4c26d23a556d7ba364881
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-11 00:28:29 +0800

    Add screens

---
+++ M15_M16_SongCollection/src/navigator/MainNavigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailScreen from '../screens/DetailScreen';
+
+const Stack = createStackNavigator();
+
+const MainNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+
+                <Stack.Screen
+                    name='Home'
+                    component={HomeScreen}
+                    options={{ headerShown: false }}
+                />
+
+                <Stack.Screen
+                    name='Detail'
+                    component={DetailScreen}
+                    options={{ headerShown: false }}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer >
+    )
+};
+
+export default MainNavigator;
---
+++ M15_M16_SongCollection/src/screens/DetailScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const DetailScreen = () => {
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default DetailScreen;
