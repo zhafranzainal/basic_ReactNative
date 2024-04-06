98c72dd0382860e27601920dc168025fce58d8d5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-04-07 07:53:46 +0800

    Create button and input components

---
+++ SocialMedia/src/components/ButtonComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export const Button = (props) => {
+
+    const { text } = props;
+
+    return (
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} {...props}>
+                <Text style={styles.text}>
+                    {text}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+
+    buttonContainer: {
+        width: '40%',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 20,
+        margin: 16,
+        backgroundColor: '#CAE3BB'
+    },
+
+    button: {
+        padding: 8
+    },
+
+    text: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    }
+
+});
