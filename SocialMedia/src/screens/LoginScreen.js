63c7182417c21c613b580e785d0e6ac6e8f2792f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-04-07 08:00:42 +0800

    Update LoginScreen.js

--- SocialMedia/src/screens/LoginScreen.js
+++ SocialMedia/src/screens/LoginScreen.js
@@ -1,14 +1,87 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Input } from '../components/InputComponent';
+import { Button } from '../components/ButtonComponent';
 
 const LoginScreen = () => {
 
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.scroll}>
+            <View style={styles.mainContainer}>
+
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.image}
+                        source={
+                            require('../../assets/images/login.png')
+                        }
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Input title="Username" placeholder="Username" />
+                </View>
+
+                <Button text="Login" />
+
+                <View style={styles.textContainer}>
+
+                    <Text style={styles.text}>Don't have an account?</Text>
+
+                    <TouchableOpacity>
+                        <Text style={styles.registerText}> Register</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+            </View>
+        </ScrollView>
     );
 
 }
+
+const styles = StyleSheet.create({
+
+    scroll: {
+        flexGrow: 1
+    },
+
+    mainContainer: {
+        backgroundColor: '#E6E6FA',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    imageContainer: {
+        marginTop: 32,
+        marginBottom: 16
+    },
+
+    image: {
+        width: 180,
+        height: 180
+    },
+
+    inputContainer: {
+        padding: 16,
+        width: '100%'
+    },
+
+    textContainer: {
+        flexDirection: 'row',
+        marginTop: 16
+    },
+
+    text: {
+        fontSize: 16
+    },
+
+    registerText: {
+        color: '#1A5B0A',
+        fontSize: 16
+    }
+
+});
 
 export default LoginScreen;
