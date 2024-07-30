cde510f741019cf5b7b646b0000f1cf68902b372
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-07-30 11:10:38 +0800

    Create store to store global state

--- SocialMedia/App.tsx
+++ SocialMedia/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainNavigator from './src/navigator/MainNavigator';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <MainNavigator />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <MainNavigator />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
