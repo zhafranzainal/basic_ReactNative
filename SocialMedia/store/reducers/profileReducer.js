0cd6b83b54b7c97c53798efe37f743c7af481606
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-07-30 11:33:40 +0800

    Add execution for action create profile

--- SocialMedia/store/reducers/profileReducer.js
+++ SocialMedia/store/reducers/profileReducer.js
@@ -5,5 +5,23 @@ const initialState = {
 };
 
 export const profileReducer = (state = initialState, action) => {
+
+    if (action.type === 'CREATE_PROFILE') {
+
+        const data = action.payload;
+        const newUsername = data.username;
+        const newEmail = data.email;
+        const newPassword = data.password;
+
+        return {
+            ...state,
+            username: newUsername,
+            email: newEmail,
+            password: newPassword
+        };
+
+    }
+
     return state;
+
 };
