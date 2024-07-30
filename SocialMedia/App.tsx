ae27e9ba7be731bf28710f328abf1f6740e969df
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-07-30 11:18:05 +0800

    Add profile reducer

--- SocialMedia/App.tsx
+++ SocialMedia/App.tsx
@@ -3,9 +3,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainNavigator from './src/navigator/MainNavigator';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { profileReducer } from './store/reducers/profileReducer';
 
 const rootReducer = combineReducers({
-
+  profileReducer: profileReducer
 });
 
 const store = createStore(rootReducer);
---
+++ SocialMedia/store/reducers/profileReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    username: "",
+    email: "",
+    password: "",
+};
+
+export const profileReducer = (state = initialState, action) => {
+    return state;
+};
