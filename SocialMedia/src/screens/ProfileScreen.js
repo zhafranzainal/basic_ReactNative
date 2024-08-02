956a11e960776143fd6ab6182eda30c8b7ef255a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-08-02 17:20:55 +0800

    Create inputs for profile screen

--- SocialMedia/src/screens/ProfileScreen.js
+++ SocialMedia/src/screens/ProfileScreen.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { Input } from '../components/InputComponent';
 
 const ProfileScreen = () => {
     return (
         <ScrollView contentContainerStyle={styles.scroll}>
             <View style={styles.mainContainer}>
+
                 <View style={styles.imageContainer}>
                     <Image
                         style={styles.image}
                         source={require('../../assets/images/avatar.png')}
                     />
                 </View>
+
+                <View style={styles.inputContainer}>
+                    <Input
+                        title="Username"
+                        editable={false}
+                    />
+                    <Input
+                        title="Email"
+                        editable={false}
+                    />
+                    <Input
+                        title="Password"
+                        editable={false}
+                    />
+                </View>
+
             </View>
         </ScrollView>
     );
@@ -34,6 +52,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'black'
     },
+    inputContainer: {
+        padding: 16,
+        width: '100%'
+    }
 });
 
 export default ProfileScreen;
