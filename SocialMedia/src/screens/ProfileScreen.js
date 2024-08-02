2809feab5a12e0aaa4823188ac3665e0b3490d52
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-08-02 16:45:17 +0800

    Add profile photo

--- SocialMedia/src/screens/ProfileScreen.js
+++ SocialMedia/src/screens/ProfileScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
 
 const ProfileScreen = () => {
     return (
-        <View>
-            <Text>ProfileScreen</Text>
-        </View>
-    )
+        <ScrollView contentContainerStyle={styles.scroll}>
+            <View style={styles.mainContainer}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.image}
+                        source={require('../../assets/images/avatar.png')}
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    );
 }
+
+const styles = StyleSheet.create({
+    scroll: {
+        flexGrow: 1
+    },
+    mainContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    imageContainer: {
+        margin: 16
+    },
+    image: {
+        width: 120,
+        height: 120,
+        borderRadius: 100,
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+});
 
 export default ProfileScreen;
