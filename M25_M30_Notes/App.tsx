37ecc1c920722f98d33f7b7de80f266606e22e3f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-08-03 15:09:19 +0800

    Create TryCodeScreen

--- M25_M30_Notes/App.tsx
+++ M25_M30_Notes/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import MainNavigator from './src/navigator/MainNavigator';
+import TryCodeScreen from './src/screens/TryCodeScreen';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <MainNavigator />
+      <TryCodeScreen />
     </SafeAreaProvider>
   );
 }
---
+++ M25_M30_Notes/src/screens/TryCodeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const TryCodeScreen = () => {
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.secondContainer}>
+                <View style={styles.blueBox} />
+                <View style={styles.redBox} />
+                <View style={styles.blueBox} />
+                <View style={styles.blueBox} />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: 'skyblue'
+    },
+    secondContainer: {
+        padding: 16,
+        margin: 16,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'mistyrose'
+    },
+    blueBox: {
+        backgroundColor: 'blue',
+        width: '25%',
+        height: '25%',
+        borderWidth: 1
+    },
+    redBox: {
+        backgroundColor: 'red',
+        width: '25%',
+        height: '25%',
+        borderWidth: 1
+    }
+});
+
+export default TryCodeScreen;
